9a542309d6930a0625c9bccb08ed397b7f77f5e7
Ulises Peguero <peguero.ulises@gmail.com>
2022-09-26 20:02:40 -0500

    Studio-5 Completed

--- src/org/launchcode/java/studios/restaurantmenu/Menu.java
+++ src/org/launchcode/java/studios/restaurantmenu/Menu.java
@@ -15,6 +15,62 @@ public class Menu {
     public Menu(Date lastModifiedDate, int version) {
         this.lastModifiedDate = lastModifiedDate;
         this.version = version;
+        items = new ArrayList<>();
+    }
+
+    /**
+     *  Adds new menu item to the menu if it doesn't exist in it already
+     *
+     * @param item      new menu item to be added
+     * @return boolean  True if added | False if failed
+     */
+    public boolean addMenuItem(MenuItem item) {
+        if(items.contains(item)) return false;
+        item.setNewItem(true);  // set item as new
+        this.items.add(item);   // add item
+        this.setLastModifiedDate(); // set last modified date to current
+        return true;
+    }
+
+    /**
+     *  Remove menu item given the shortname
+     *
+     * @param shortName     shortName given to the menu item
+     * @return  MenuItem    Item that has been removed or null if it wasn't found
+     */
+    public MenuItem removeItem(String shortName) {
+        MenuItem item = getItemByShortName(shortName);
+        if(item != null) items.remove(item);
+        return item;
+    }
+
+    /**
+     *  Search MenuItem by the shortname
+     *
+     * @param shortName     shortName given to the menu item
+     * @return  MenuItem    Item found or null if it wasn't found
+     */
+    public MenuItem getItemByShortName(String shortName) {
+        return items.stream()
+                .filter(listItem -> shortName.toLowerCase().equals(listItem.getShortName().toLowerCase()))
+                .findAny()
+                .orElse(null);
+    }
+
+    /**
+     *  Sets a date to check new item flag on menu items in the menu
+     *
+     * @param thresholdDate  Any item set after and on this date will be flag as new
+     */
+    public void setNewItemFlagToDate(Date thresholdDate) {
+        for(MenuItem item: this.items) {
+            item.setNewItem(!item.getCreationDate().before(thresholdDate));
+        }
+    }
+
+    // when no date is provided we use the lastModifiedDate
+    public void setNewItemFlagToDate() {
+        setNewItemFlagToDate(lastModifiedDate);
     }
 
     public void setLastModifiedDate() {
@@ -40,24 +96,6 @@ public class Menu {
 
     private void increaseVersion() {
         version++;
-    }
-
-    public boolean addItem(MenuItem item) {
-        if(getItemByShortName(item.getShortName()) != null) return false;
-        items.add(item);return true;
-    }
-
-    public MenuItem removeItem(String shortName) {
-        MenuItem item = getItemByShortName(shortName);
-        if(item != null)items.remove(item);
-        return item;
-    }
-
-    public MenuItem getItemByShortName(String shortName) {
-        return items.stream()
-                .filter(listItem -> shortName.equals(listItem.getShortName()))
-                .findAny()
-                .orElse(null);
     }
 
     @Override
